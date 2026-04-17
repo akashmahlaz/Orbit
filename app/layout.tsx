1f9063d30284d817fd216705d988719eb36ce231
akashmahlaz <akashdalla408@gmail.com>
2026-04-17 13:16:58 +0530

    add Tailwind CSS integration, update layout and metadata, and modify home page structure

---
+++ app/layout.tsx
@@ -0,0 +1,23 @@
+import { Metadata } from "next";
+import {Geist, Geist_Mono} from "next/font/google"
+import "./global.css"
+
+
+const geistSans = Geist({variable:"--font-geist-sans", subsets:["latin"]})
+const geist_mono = Geist_Mono({variable:"--font-geist-mono", subsets:["latin"]})
+
+export const metadata:Metadata = {
+    title:"Orbiz : Esay Lead Management and project Management ",
+    description:"Orbit is a unified project management and CRM platform built for modern developers, freelancers, and agencies who need complete clarity across project execution and client acquisition. It combines project tracking, GitHub-based development visibility, and lead management into a single clean interface. Designed to eliminate decision fatigue, Orbit helps users prioritize tasks, manage client relationships, track leads from outreach to conversion, and make data-driven decisions every day.",
+    authors:{ name:"Akashdeep Singh, Email: akashdalla406@gmail.com", url:"https.orbitz.in" }
+}
+
+export default function Rootlayout({children,}:Readonly<{children:React.ReactNode;}>){
+    return(
+         <html lang="en">
+        <body className={`${geistSans.variable}, ${geist_mono.variable}`}>
+            {children}
+        </body>
+       </html> 
+    )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home(){
     return(
-        <div>
-            <h1>THIS IS OUR CRM</h1>
+        <div >
+            
         </div>
     )
 }
